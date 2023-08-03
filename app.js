149ce9698f2799941e878e68c414dc39bbd11eeb
Naweed Ahmadi <naweed.ahmadi2010@gmail.com>
2023-08-03 00:04:06 -0700

    mongoose-encryption has been added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
@@ -15,6 +16,9 @@ const userSchema = new mongoose.Schema({
     userName: String,
     password: String
 })
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
